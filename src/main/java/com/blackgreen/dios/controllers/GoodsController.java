2c634c1bffb387a7e2d96b8d1b6a76544965cab7
oyeeun <eun8548@naver.com>
2023-01-03 12:51:43 +0900

    윤정이꺼 합침

--- src/main/java/com/blackgreen/dios/controllers/GoodsController.java
+++ src/main/java/com/blackgreen/dios/controllers/GoodsController.java
@@ -4,10 +4,12 @@ import com.blackgreen.dios.entities.member.UserEntity;
 import com.blackgreen.dios.entities.store.*;
 import com.blackgreen.dios.enums.CommonResult;
 import com.blackgreen.dios.enums.goods.AddReviewResult;
+import com.blackgreen.dios.models.PagingModel;
 import com.blackgreen.dios.services.GoodsService;
 import com.blackgreen.dios.services.RollbackException;
 import com.blackgreen.dios.vos.GoodsVo;
 import com.blackgreen.dios.vos.ReviewVo;
+import jdk.jfr.Category;
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.Collection;
 
 @Controller(value = "com.blackgreen.dios.controllers.GoodsController")
 @RequestMapping(value = "/goods")
@@ -46,6 +49,8 @@ public class GoodsController {
 
         return modelAndView;
     }
+
+
 
     @PostMapping(value = "write")
     @ResponseBody
@@ -86,10 +91,9 @@ public class GoodsController {
         JSONObject responseObject = new JSONObject();
         System.out.println("check image" + images);
 
-
         for (SizeEntity size : sizes) {
             System.out.println("size check" + size.getId());
-            size.setItemIndex(item.getIndex());//인덱스에 저장한 후 size 에 insert
+            size.setItemIndex(item.getIndex());
         }
 
 //        for (ItemColorEntity color:colors) {
@@ -98,7 +102,7 @@ public class GoodsController {
 //            System.out.println(color.getItemIndex());
 //        }
 
-        responseObject.put("gin", item.getIndex()); //gin는 goodsIndex 의 줄임말이다.
+        responseObject.put("gid", item.getIndex()); //gid는 goodsIndex 의 줄임말이다.
         responseObject.put("result", result.name().toLowerCase());
         return responseObject.toString();
     }
@@ -114,7 +118,6 @@ public class GoodsController {
         // 프젝하다가 업로드, 다운로드 할 일이 있는데 이걸 교과서처럼 참고하삼요~!!!
         return new ResponseEntity<>(image.getData(), headers, HttpStatus.OK);
     }
-
 
     @RequestMapping(value = "image",
             method = RequestMethod.POST, // 업로드를 위한 것
@@ -136,6 +139,57 @@ public class GoodsController {
     }
 
 
+
+    //수정 페이지
+    //타임리프 쓸때는 responseBody 안붙임 !!!
+    @RequestMapping(value = "modify",
+            method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+
+    public ModelAndView getModify(@SessionAttribute(value = "user", required = false) UserEntity user,
+                                  @RequestParam(value = "gid", required = false) int gid) {
+
+        ModelAndView modelAndView = new ModelAndView("goods/modify");
+        ItemEntity item = new ItemEntity();
+        item.setIndex(gid);
+
+        Enum<?> result = this.goodsService.prepareModifyItem(item, user);
+        modelAndView.addObject("item", item);
+        modelAndView.addObject("result", result.name());
+        ItemColorEntity[] colors = this.goodsService.getColor();
+        SellerEntity[] sellers = this.goodsService.getSeller();
+        SizeEntity[] sizes = this.goodsService.getSize();
+        ItemCategoryEntity[] categories = this.goodsService.getItemCategory();
+        modelAndView.addObject("color", colors);
+        modelAndView.addObject("size", sizes);
+        modelAndView.addObject("seller", sellers);
+        modelAndView.addObject("category", categories);
+        if (result == CommonResult.SUCCESS) {   // html 에 넘겨줄려고
+            modelAndView.addObject("gid", item.getIndex());
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "modify",
+            method = RequestMethod.PATCH,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody //xhr 로 반환 받을 것들은 무조건 ResponseBody 붙여준다.
+    public String patchModify(@SessionAttribute(value = "user", required = false) UserEntity user,
+                              @RequestParam(value = "gid") int gid,
+                              @RequestParam(value = "images", required = false) MultipartFile images,
+                              ItemEntity item) throws IOException {
+        item.setIndex(gid);
+        Enum<?> result = this.goodsService.ModifyItem(item, user,images);
+        JSONObject responseObject = new JSONObject();
+        responseObject.put("result", result.name().toLowerCase());
+        if (result == CommonResult.SUCCESS) {
+            responseObject.put("gid", gid);
+        }
+        return responseObject.toString();
+    }
+
+
+
     @RequestMapping(value = "read",
             method = RequestMethod.GET,
             produces = MediaType.TEXT_HTML_VALUE)
@@ -144,6 +198,17 @@ public class GoodsController {
 
         ModelAndView modelAndView = new ModelAndView("goods/read");
         GoodsVo goods = this.goodsService.getItem(gid);
+
+        // postWrite 에서 저장한 사이즈 값을 가져오는 구문
+        SizeEntity[] sizes = new SizeEntity[this.goodsService.getSize().length];
+        int count = 0;
+        for (int i = 0; i <sizes.length ; i++) {
+            SizeEntity size = this.goodsService.getItemSize(gid);
+            count =+ 1;
+            sizes[i] = size;
+        }
+
+        modelAndView.addObject("sizes", sizes);
 
 
 //        ItemColorEntity[] colors = new ItemColorEntity[goods.getColors().length];
@@ -162,10 +227,29 @@ public class GoodsController {
         modelAndView.addObject("category", this.goodsService.getCategory(goods.getCategoryId()));
         modelAndView.addObject("seller", this.goodsService.getBrand(goods.getSellerIndex()));
 
+
 //        modelAndView.addObject("color", this.goodsService.getItemColor(goods.getColors()));
 //        modelAndView.addObject("Size", goods.getSizes());
 
         return modelAndView;
+    }
+
+    @RequestMapping(value = "read",
+            method = RequestMethod.DELETE,//DELETE로 사용한 이유: 주소를 동일하게 하고 방식을 달리 하는 것은 레스트인데,그냥 삭제할때 이렇게 쓰기로 개발자끼리 약속함
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody //return 값을 그 자체로 넘기기 위해 보냄
+    public String deleteRead(@SessionAttribute(value = "user", required = false) UserEntity user,
+                             @RequestParam(value = "gid") int gid) {
+        ItemEntity item = new ItemEntity();
+        item.setIndex(gid);
+
+        Enum<?> result = this.goodsService.deleteItem(item, user);
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("result", result.name().toLowerCase());
+        if (result == CommonResult.SUCCESS) {
+            responseJson.put("cad", item.getCategoryId());
+        }
+        return responseJson.toString();
     }
 
     @GetMapping(value = "titleImage") // 다운로드용 맵핑
@@ -202,8 +286,9 @@ public class GoodsController {
 
     @DeleteMapping(value = "review", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String deleteReview( ReviewEntity review) {
-        Enum<?> result = this.goodsService.deleteReview(review);
+    public String deleteReview( @SessionAttribute(value = "user", required = false) UserEntity user,
+                                ReviewEntity review) {
+        Enum<?> result = this.goodsService.deleteReview(user,review);
         JSONObject responseJson = new JSONObject();
         responseJson.put("result", result.name().toLowerCase());
         return responseJson.toString();
@@ -212,8 +297,9 @@ public class GoodsController {
     @PatchMapping(value = "review",produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
 
-    public String patchRecoverComment(@SessionAttribute(value = "user", required = false) UserEntity user, ReviewEntity review) {
+    public String patchRecoverReview(@SessionAttribute(value = "user", required = false) UserEntity user, ReviewEntity review) {
         Enum<?> result = this.goodsService.recoverReview(review, user);
+        System.out.println(result);
         JSONObject responseObject = new JSONObject();
         responseObject.put("result", result.name().toLowerCase());
         return responseObject.toString();
@@ -222,9 +308,8 @@ public class GoodsController {
 
     @PostMapping(value = "review")
     @ResponseBody
-    public String postReview(UserEntity user, @RequestParam(value = "images", required = false) MultipartFile[] images, ReviewEntity review) throws IOException, RollbackException {
+    public String postReview(@SessionAttribute(value = "user", required = false) UserEntity user, @RequestParam(value = "images", required = false) MultipartFile[] images, ReviewEntity review) throws IOException, RollbackException {
         JSONObject responseObject = new JSONObject();
-        user.setEmail("liz_0710@naver.com");
         Enum<?> result;
         try {
             result = this.goodsService.addReview(user, review, images);
@@ -249,6 +334,35 @@ public class GoodsController {
             responseEntity = new ResponseEntity<>(reviewImage.getData(), HttpStatus.OK);
         }
         return responseEntity;
+    }
+
+    @GetMapping(value = "list")
+    public ModelAndView getList(@RequestParam(value = "cad") String cad, //카테고리 아이디
+                                @RequestParam(value = "page", required = false,
+                                        defaultValue = "1") Integer page,
+                                @RequestParam(value = "criterion", required = false) String criterion,
+                                @RequestParam(value = "keyword", required = false) String keyword) {
+        page = Math.max(1, page);
+        ModelAndView modelAndView = new ModelAndView("store/list");
+
+        ItemCategoryEntity[] categories = this.goodsService.getItemCategory();
+
+        modelAndView.addObject("categories",categories);
+
+        ItemCategoryEntity category = this.goodsService.getCategory(cad);
+        modelAndView.addObject("category", category.getText()); // id값으로 카테고리 가져오기
+
+        int totalCount; //pagination 하려고 가져온 것
+        if ( category != null) {
+            totalCount = this.goodsService.getItemCount(category, criterion, keyword);
+            PagingModel paging = new PagingModel(totalCount, page);
+            modelAndView.addObject("paging", paging);
+
+            GoodsVo[] goods = this.goodsService.getItems(category, paging,criterion,keyword);
+            modelAndView.addObject("goods", goods);
+        }
+
+        return modelAndView;
     }
 
 }
